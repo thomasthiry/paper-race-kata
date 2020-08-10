5589c2e01f46867f174f4aded5489c02398d58b5
Mob <remote-coding-dojo@dojo.com>
2020-08-10 22:02:37 +0200

    Used builder for inertia test

--- PaperRaceKata/PositionBehavior.cs
+++ PaperRaceKata/PositionBehavior.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using static PaperRaceKata.CarBuilder;
 
 namespace PaperRaceKata
 {
@@ -7,17 +8,51 @@ namespace PaperRaceKata
         [Fact]
         public void New_car_is_at_position_zero()
         {
-            var car = new Car(new Inertia(-1, -1), new Position(0,0));
+            var car = ACar()
+                .With(new Position(0,0))
+                .Build();
 
             Assert.Equal(new Position(0, 0), car.Position);
         }
+
         [Fact]
         public void Car_without_inertia_and_adjustment_center_remains_at_the_same_location()
         {
-            var car = new Car(new Inertia(0, 0), new Position(1, 1))
+            var expectedPosition = new Position(1, 1);
+            var car = ACar()
+                    .With(new Inertia(0, 0))
+                    .With(expectedPosition).Build()
                 .With(Adjustment.Center);
              
-            Assert.Equal(new Position(1, 1), car.Position);
+            Assert.Equal(expectedPosition, car.Position);
+        }
+    }
+
+    public class CarBuilder
+    {
+        private Position _position = new Position(0, 0);
+        private Inertia _inertia = new Inertia(0, 0);
+
+        public static CarBuilder ACar()
+        {
+            return new CarBuilder();
+        }
+
+        public CarBuilder With(Position position)
+        {
+            this._position = position;
+            return this;
+        }
+
+        public Car Build()
+        {
+            return new Car(_inertia, _position);
+        }
+
+        public CarBuilder With(Inertia inertia)
+        {
+            _inertia = inertia;
+            return this;
         }
     }
 }
