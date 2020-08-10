e309aea2543944e513565ec1cfbd67c64e26e37e
Mob <remote-coding-dojo@dojo.com>
2020-08-10 21:27:59 +0200

    Added Car_without_inertia_and_adjustment_center_remains_at_the_same_location

--- PaperRaceKata/Car.cs
+++ PaperRaceKata/Car.cs
@@ -8,12 +8,18 @@ namespace PaperRaceKata
             Position = new Position(0, 0);
         }
 
+        public Car(Inertia inertia, Position position)
+        {
+            Inertia = inertia;
+            Position = position;
+        }
+
         public Inertia Inertia { get; }
         public Position Position { get; }
 
         public Car With(Adjustment adjustment)
         {
-            return new Car(Inertia.Add(Inertia.DirectionFor(adjustment)));
+            return new Car(Inertia.Add(Inertia.DirectionFor(adjustment)), Position);
         }
     }
 }
--- PaperRaceKata/PositionBehavior.cs
+++ PaperRaceKata/PositionBehavior.cs
@@ -11,5 +11,13 @@ namespace PaperRaceKata
 
             Assert.Equal(new Position(0, 0), car.Position);
         }
+        [Fact]
+        public void Car_without_inertia_and_adjustment_center_remains_at_the_same_location()
+        {
+            var car = new Car(new Inertia(0, 0), new Position(1, 1))
+                .With(Adjustment.Center);
+             
+            Assert.Equal(new Position(1, 1), car.Position);
+        }
     }
 }
