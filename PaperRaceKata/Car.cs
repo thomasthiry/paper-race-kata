80ae1a9f491b211c15273aea936472530ff21df8
Thomas Thiry <thomas.thiry@gmail.com>
2020-08-24 21:26:59 +0200

    added more tests to verify inertia

--- PaperRaceKata/Car.cs
+++ PaperRaceKata/Car.cs
@@ -13,10 +13,11 @@ namespace PaperRaceKata
 
         public Car Apply(Adjustment adjustment)
         {
-            var positionX = Position.x + Inertia._x;
-            var positionY = Position.y + Inertia._y;
+            var inertia = Inertia.Add(Inertia.DirectionFor(adjustment));
+            var positionX = Position.x + inertia._x;
+            var positionY = Position.y + inertia._y;
             var position = new Position(positionX, positionY);
-            return new Car(Inertia.Add(Inertia.DirectionFor(adjustment)), position);
+            return new Car(inertia, position);
         }
     }
 }
