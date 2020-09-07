14479117e74dacd35c059acb67104e522e41c8e1
Thomas Thiry <thomas.thiry@gmail.com>
2020-09-07 20:20:39 +0200

    Add SignalR server side

---
+++ PaperRaceKata.Web/Hubs/CarHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace PaperRaceKata.Web.Hubs
+{
+    public class CarHub : Hub
+    {
+        public async Task Adjust(string carId, string direction)
+        {
+            await Clients.All.SendAsync("CarAdjusted", carId, direction);
+        }
+    }
+}
