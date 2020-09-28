b84165468368f5c415f25236222a0edfb21d2793
Thomas Thiry <thomas.thiry@gmail.com>
2020-09-28 21:51:38 +0200

    remove adjustment using dropdown

--- PaperRaceKata.Web/wwwroot/js/paperRace.js
+++ PaperRaceKata.Web/wwwroot/js/paperRace.js
@@ -2,9 +2,6 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/carhub").build();
 
-//Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
-
 connection.on("CarAdjusted", function (carId, direction, position) {
     var message = carId + " was adjusted to the " + direction + " at position " + position;
     var li = document.createElement("li");
@@ -16,19 +13,8 @@ connection.on("RaceReset", function () {
     document.getElementById("adjustments").innerHTML = "";
 });
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
+connection.start().catch(function (err) {
     return console.error(err.toString());
-});
-
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var carId = document.getElementById("carIdInput").value;
-    var direction = document.getElementById("directionInput").value;
-    connection.invoke("Adjust", carId, direction).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
 });
 
 document.getElementById("resetButton").addEventListener("click", function (event) {
