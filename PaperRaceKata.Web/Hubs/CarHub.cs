8eedf1876a3b791a136e61874fe5dc766abf6786
Thomas Thiry <thomas.thiry@gmail.com>
2020-09-28 21:10:32 +0200

    Ensure the car is maintained between adjustements

--- PaperRaceKata.Web/Hubs/CarHub.cs
+++ PaperRaceKata.Web/Hubs/CarHub.cs
@@ -6,11 +6,11 @@ namespace PaperRaceKata.Web.Hubs
     public class CarHub : Hub
     {
 
-        Car car = new Car(new Inertia(0,0), new Position(0,0));
+        private static Car car = new Car(new Inertia(0,0), new Position(0,0));
 
         public async Task Adjust(string carId, Adjustment direction)
         {
-            car.Apply(direction);
+            car = car.Apply(direction);
 
             await Clients.All.SendAsync("CarAdjusted", carId, direction, car.Position.ToString());
         }
