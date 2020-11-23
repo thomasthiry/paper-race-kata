02f22a5a050a917b27cee4d656dbc9dcb9874095
Thomas Thiry <thomas.thiry@gmail.com>
2020-11-23 21:22:47 +0100

    backend keep track of all cars

--- PaperRaceKata.Web/Hubs/CarHub.cs
+++ PaperRaceKata.Web/Hubs/CarHub.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace PaperRaceKata.Web.Hubs
@@ -6,27 +7,25 @@ namespace PaperRaceKata.Web.Hubs
     public class CarHub : Hub
     {
 
-        private static Car car = new Car(new Inertia(0,0), new Position(0,0));
+        private static Dictionary<string, Car> cars = new Dictionary<string, Car>();
 
-        public override async Task OnConnectedAsync()
-        {
-            await Clients.Caller.SendAsync("CarAdjusted", "red car", Adjustment.Center, car.Position);
-        }
 
         public async Task JoinGame(string carId)
         {
-            await Clients.All.SendAsync("CarJoined", carId, car.Position);
+            var newCar = new Car(new Inertia(0, 0), new Position(0, 0));
+            cars.Add(carId, newCar);
+            await Clients.All.SendAsync("CarJoined", carId, newCar.Position);
         }
         public async Task Adjust(string carId, Adjustment direction)
         {
-            car = car.Apply(direction);
+            cars[carId] = cars[carId].Apply(direction);
 
-            await Clients.All.SendAsync("CarAdjusted", carId, direction, car.Position);
+            await Clients.All.SendAsync("CarAdjusted", carId, direction, cars[carId].Position);
         }
 
         public async Task Reset()
         {
-            car = new Car(new Inertia(0, 0), new Position(0, 0));
+            cars = new Dictionary<string, Car>();
 
             await Clients.All.SendAsync("RaceReset");
         }
